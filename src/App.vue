ca179f5e1cc776cd1d56032ef3efac8667563fa8
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2021-04-25 09:52:29 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navigation>
+    <router-view />
+  </Navigation>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style>
+</style>
